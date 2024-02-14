df5248a8f6391eee2866c7c459921fe5709f920b
Dmitry Rakovich <dmitryrakovich@mail.ru>
2024-02-14 09:39:31 +0300

    fix:

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,5 @@
 import { Calendar } from './Calendar'
 import { Exit } from './Exit'
 import { Profile } from './Profile'
-import { Logo } from './Logo'
 
-export { Calendar, Exit, Logo, Profile }
+export { Calendar, Exit, Profile }

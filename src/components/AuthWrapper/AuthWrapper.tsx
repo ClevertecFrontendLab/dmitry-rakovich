d18061605ea906d15c4f1f1572f25eb85979e020
Dmitry Rakovich <dmitryrakovich@mail.ru>
2024-02-27 23:48:39 +0300

    fix: remove return

--- src/components/AuthWrapper/AuthWrapper.tsx
+++ src/components/AuthWrapper/AuthWrapper.tsx
@@ -1,11 +1,8 @@
 import { PropsWithChildren } from "react"
 import styles from './styles.module.scss'
 
-export const AuthWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.blur}></div>
-            {children}
-        </div>
-    )
-}
+export const AuthWrapper: React.FC<PropsWithChildren> = ({ children }) =>
+    <div className={styles.wrapper}>
+        <div className={styles.blur}></div>
+        {children}
+    </div>

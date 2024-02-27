bc37941ab03f52fe5ff295b4d141dbdc61937b73
Dmitry Rakovich <dmitryrakovich@mail.ru>
2024-02-28 01:12:46 +0300

    fix: URLs

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,12 +1,21 @@
 export const BASE_URL = 'https://marathon-api.clevertec.ru'
 
+const URLs = {
+    auth_google: '/auth/google',
+    auth_login: '/auth/login',
+    auth_registration: '/auth/registration',
+    auth_check_email: '/auth/check-email',
+    auth_confirm_email: '/auth/confirm-email',
+    auth_change_password: '/auth/change-password',
+}
+
 export const api = {
     auth: {
-        google: `${BASE_URL}/auth/google`,
-        login: `${BASE_URL}/auth/login`,
-        registration: `${BASE_URL}/auth/registration`,
-        check_email: `${BASE_URL}/auth/check-email`,
-        confirm_email: `${BASE_URL}/auth/confirm-email`,
-        change_password: `${BASE_URL}/auth/change-password`,
+        google: `${BASE_URL}/${URLs.auth_google}`,
+        login: `${BASE_URL}/${URLs.auth_login}`,
+        registration: `${BASE_URL}/${URLs.auth_registration}`,
+        check_email: `${BASE_URL}/${URLs.auth_check_email}`,
+        confirm_email: `${BASE_URL}/${URLs.auth_confirm_email}`,
+        change_password: `${BASE_URL}/${URLs.auth_change_password}`,
     },
 }
